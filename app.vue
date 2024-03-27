e4157b6d820e3fed747d0b3f08976af7f02fb64d
Alejandro Mery <amery@jpi.io>
2024-03-27 15:17:35 +0000

    add base for reactive links case
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- app.vue
+++ app.vue
@@ -1,13 +1,35 @@
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
-        </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
-    </UCard>
-  </UContainer>
+  <UDashboardLayout>
+    <UDashboardPanel>
+      <UDashboardPanelContent>
+        <UDashboardCard :title="title" :links="links">
+          <UTable :loading="pending" :columns="columns" :rows="data?.products" />
+        </UDashboardCard>
+      </UDashboardPanelContent>
+    </UDashboardPanel>
+  </UDashboardLayout>
 </template>
+
+<script setup lang="ts">
+interface product {}
+interface products {
+	products: product[];
+}
+
+const title = ref("title")
+const columns = [{key: 'id'}, {key: 'title'}]
+
+const { data, pending, refresh } = await useFetch<products>('https://dummyjson.com/products')
+
+const links = [{
+	label: 'refresh',
+	click: refresh,
+	loading: pending,
+	icon: 'i-material-symbols-refresh',
+	variant: 'ghost',
+}, {
+	icon: 'i-material-symbols-download',
+	disabled: pending,
+	click: () => { alert("save") },
+}]
+</script>
